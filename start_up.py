24ee3350fba9d4158b4a7626052a57064be582c1
donghaixing <1413470138@qq.com>
2019-05-03 01:51:55 +0800

    change server start_up location.

--- start_up.py
+++ start_up.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2019/5/3 1:19
+# @Time    : 2019/5/3 1:49
 # @File    : start_up.py
 # @Author  : donghaixing
 # Do have a faith in what you're doing.
 # Make your life a story worth telling.
 
-from .server import tornado_server as server
+from __future__ import absolute_import
+
+from ork.server import tornado_server as server
 
 if __name__ == '__main__':
     server.run()
